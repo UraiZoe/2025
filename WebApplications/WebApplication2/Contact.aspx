e466ee15ddf4376158555198b5115d2cf241ba5e
UraiZoe <urazoe366@hengersor.hu>
2025-01-07 08:27:47 +0100

    órai munka
    2025.01.07.

---
+++ WebApplications/WebApplication2/Contact.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="Oldal" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="WebApplication2.Contact" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main aria-labelledby="title">
+        <h2 id="title"><%: Title %></h2>
+        <h3>Zojé</h3>
+        <address>
+            My way or the highway.<br />
+            GyÁ<br />
+            <abbr title="Phone">P:</abbr>
+            Telephone
+        </address>
+
+        <address>
+        <asp:TextBox ID="textHost" runat="server"></asp:TextBox>
+        <asp:TextBox ID="textPort" runat="server"></asp:TextBox>
+        <asp:TextBox ID="textDatabase" runat="server"></asp:TextBox>
+        <asp:TextBox ID="textUser" runat="server"></asp:TextBox>
+        <asp:TextBox ID="textPass" runat="server"></asp:TextBox>
+
+
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Esemeny"></asp:Button>
+        <asp:GridView ID="dataGridView1" runat="server"></asp:GridView>
+
+            <strong><a href="https://www.paypal.com/paypalme/MartinGamer2019">Supportáld Martint</a></strong><br />
+           </address>
+    </main>
+</asp:Content>
---
+++ WebApplications/WebApplication2/Contact.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication2
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            textHost.Text = "localhost";
+            textPort.Text = "3307";
+            textUser.Text = "root";
+            textPass.Text = "pass";
+            textDatabase.Text = "transactions";
+
+        }
+        protected void Esemeny(object sender, EventArgs e)
+        {
+            // Ide jön az eseménykezelő logika
+            Response.Write("<p>Esemény sikeresen lefutott!</p>");
+
+            string ConnectionString = $"server={textHost.Text};port={textPort.Text};database={textDatabase.Text};user={textUser.Text};password={textPass.Text};";
+            MySqlConnection MyConn2 = new MySqlConnection(ConnectionString);
+            string sqlQuery = $"select * from {textDatabase.Text}.accounts;";
+            try
+            {
+                MyConn2.Open();
+                MySqlCommand MyCommand2 = new MySqlCommand(sqlQuery, MyConn2);
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+                DataTable dTable = new DataTable();
+                MyAdapter.Fill(dTable);
+                dataGridView1.DataSource = dTable;
+                dataGridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+
+                //MessageBox.Show($"Hiba az adatbázis kapcsolódásakor: {ex.Message}");
+                //MessageBox.Show($"Hiba az adatbázis kapcsolódásakor: {ex.Message}", "MySQL kapcsolódási hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+
+}
